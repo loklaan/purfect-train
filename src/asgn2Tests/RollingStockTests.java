a654426ec861f781fcbad9bbc2b34f026a89c607
Lochlan Bunn <loklaan@gmail.com>
2013-05-17 18:53:28 +1000

    Initial FreightCar tests

--- src/asgn2Tests/RollingStockTests.java
+++ src/asgn2Tests/RollingStockTests.java
@@ -4,16 +4,107 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
+import asgn2Exceptions.TrainException;
+import asgn2RollingStock.*;
+
 /**
- * Tests for the Carriage classes in and for RollingStock package
+ * Tests for the various <em>Carriage</em> classes in the
+ * <strong>RollingStock</strong> package
+ * 
  * @author Lochlan Bunn - 8509719
- *
+ * 
  */
 public class RollingStockTests {
+	private RollingStock testRollingStock;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	// CONSTANTS
+	final Integer DEFAULT_GROSS_WEIGHT = 100;
+	final String DEFAULT_FREIGHT_TYPE = "D";
+
+	/*
+	 * extends RollingStock shared tests
+	 */
+
+	/**
+	 * The car should not be able to have a negative tonnage.
+	 * 
+	 * @throws TrainException
+	 */
+	@Test(expected = TrainException.class)
+	public void testConstructFreightCarWithNegativeGrossWeight()
+			throws TrainException {
+		Integer negGrossWeight = DEFAULT_GROSS_WEIGHT * -1;
+		testRollingStock = new FreightCar(negGrossWeight, DEFAULT_FREIGHT_TYPE);
 	}
 
+	/**
+	 * The car should have to weigh <em>something</em>.
+	 * 
+	 * @throws TrainException
+	 */
+	@Test(expected = TrainException.class)
+	public void testConstructFreightCarWithZeroGrossWeight()
+			throws TrainException {
+		Integer zeroGrossWeight = 0;
+		testRollingStock = new FreightCar(zeroGrossWeight, DEFAULT_FREIGHT_TYPE);
+	}
+
+	/*
+	 * Freight Car specific tests
+	 */
+
+	/**
+	 * The freight can only carry goods of a certain type, and they should be
+	 * characterised correcty with specific character representation:<br/>
+	 * <br/>
+	 * "G" - General goods<br/>
+	 * "R" - Refrigerated goods<br/>
+	 * "D" - Dangerous materials
+	 * 
+	 * @throws TrainException
+	 */
+	@Test
+	public void testConstructFreightCarWithValidGoodsType()
+			throws TrainException {
+		testRollingStock = new FreightCar(DEFAULT_GROSS_WEIGHT, "G");
+		testRollingStock = new FreightCar(DEFAULT_GROSS_WEIGHT, "R");
+		testRollingStock = new FreightCar(DEFAULT_GROSS_WEIGHT, "D");
+	}
+
+	/**
+	 * Freight with incorrectly specified good types should throw an Exception.
+	 * 
+	 * @throws TrainException
+	 */
+	@Test(expected = TrainException.class)
+	public void testConstructFreightCarWithInValidGoodsType()
+			throws TrainException {
+		testRollingStock = new FreightCar(DEFAULT_GROSS_WEIGHT, "O");
+		// TODO make this go through every character, or atleast alot?
+	}
+
+	@Test
+	public void testFreightCarReturnValidGoodsType() throws TrainException {
+		testRollingStock = new FreightCar(DEFAULT_GROSS_WEIGHT,
+				DEFAULT_FREIGHT_TYPE);
+		assertEquals("Return of valid goods type", DEFAULT_FREIGHT_TYPE,
+				testRollingStock.goodsType());
+	}
+
+	@Test(expected = TrainException.class)
+	public void testFreightCarReturnValidGoodsTypeAgainstInvalidGoodsType()
+			throws TrainException {
+		testRollingStock = new FreightCar(DEFAULT_GROSS_WEIGHT,
+				DEFAULT_FREIGHT_TYPE);
+		assertFalse("Return of valid goods type against a invalid comparison",
+				testRollingStock.goodsType() == "O");
+	}
+
+	// copy of test
+	@Test
+	public void testtest() throws TrainException {
+
+	}
 }
