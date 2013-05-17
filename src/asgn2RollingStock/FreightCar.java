8a1c50c5da4865ca093675f53c466e6097aa5c24
Murray Coggan <muzza74@gmail.com>
2013-05-17 20:20:39 +1000

    Inital FreightCar class
    Passed tests.
    
    @loklaan No tests for toString to code against.

--- src/asgn2RollingStock/FreightCar.java
+++ src/asgn2RollingStock/FreightCar.java
@@ -1,5 +1,48 @@
 package asgn2RollingStock;
 
+import asgn2Exceptions.TrainException;
+
+/**
+ * Freight cars are designed to handle a variety of goods. For the purposes of
+ * this assignment we assume there are three freight car types of interest,
+ * characterised by the kinds of goods they are designed to carry:<br/>
+ * <br/>
+ * "G" - General goods<br/>
+ * "R" - Refrigerated goods<br/>
+ * "D" - Dangerous materials
+ * 
+ * @author Murray Coggan - 8291951
+ * 
+ */
 public class FreightCar extends RollingStock {
+	private String goodsType;
+
+	public FreightCar(Integer grossWeight, String goodsType)
+			throws TrainException {
+		super(grossWeight);
+		if (!isValidGoodsType(goodsType)) {
+			throw new TrainException("The goodsType must be valid");
+		}
+		this.goodsType = goodsType;
+	}
+
+	/**
+	 * Returns the type of goods this carriage was designed to carry.
+	 * 
+	 * @return the goodsType (G", "R" or "D")
+	 */
+	public String goodsType() {
+		return this.goodsType;
+	}
+
+	private boolean isValidGoodsType(String goodsType) {
+		return goodsType == "G" || goodsType == "R" || goodsType == "D";
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
