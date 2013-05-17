79045975119ffd7aac3ccf77e63d55ad0598519f
Lochlan Bunn <loklaan@gmail.com>
2013-05-17 22:04:37 +1000

    Initial Locomotive tests

--- src/asgn2Tests/RollingStockTests.java
+++ src/asgn2Tests/RollingStockTests.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import com.sun.xml.internal.bind.v2.TODO;
-
 import asgn2Exceptions.TrainException;
 import asgn2RollingStock.*;
 
@@ -19,37 +17,49 @@ import asgn2RollingStock.*;
 public class RollingStockTests {
 	private RollingStock testRollingStock;
 	private FreightCar testFreightCar;
+	private Locomotive testLocomotive;
 
 	// CONSTANTS
 	final Integer DEFAULT_GROSS_WEIGHT = 100;
+	final Integer DEFAULT_NEGATIVE_GROSS_WEIGHT = -100;
+	final Integer ZERO = 0;
 	final String DEFAULT_FREIGHT_TYPE = "D";
+	final Integer DEFAULT_LOCO_POWER = 4;
+	final String DEFAULT_LOCO_ENGINE = "E";
+	final String DEFAULT_LOCO_CLASS = DEFAULT_LOCO_POWER + DEFAULT_LOCO_ENGINE;
 
 	/*
 	 * extends RollingStock shared tests
 	 */
 
-	/**
-	 * The car should not be able to have a negative tonnage.
-	 * 
-	 * @throws TrainException
-	 */
+	// The cars should not be able to have a negative tonnage.
+
 	@Test(expected = TrainException.class)
 	public void testConstructFreightCarWithNegativeGrossWeight()
 			throws TrainException {
-		Integer negGrossWeight = DEFAULT_GROSS_WEIGHT * -1;
-		testFreightCar = new FreightCar(negGrossWeight, DEFAULT_FREIGHT_TYPE);
+		testFreightCar = new FreightCar(DEFAULT_NEGATIVE_GROSS_WEIGHT,
+				DEFAULT_FREIGHT_TYPE);
 	}
 
-	/**
-	 * The car should have to weigh <em>something</em>.
-	 * 
-	 * @throws TrainException
-	 */
+	@Test(expected = TrainException.class)
+	public void testConstructLocomotiveWithNegativeGrossWeight()
+			throws TrainException {
+		testLocomotive = new Locomotive(DEFAULT_NEGATIVE_GROSS_WEIGHT,
+				DEFAULT_LOCO_CLASS);
+	}
+
+	// The cars should have to weigh <em>something</em>.
+
 	@Test(expected = TrainException.class)
 	public void testConstructFreightCarWithZeroGrossWeight()
 			throws TrainException {
-		Integer zeroGrossWeight = 0;
-		testFreightCar = new FreightCar(zeroGrossWeight, DEFAULT_FREIGHT_TYPE);
+		testFreightCar = new FreightCar(ZERO, DEFAULT_FREIGHT_TYPE);
+	}
+
+	@Test(expected = TrainException.class)
+	public void testConstructLocomotiveWithZeroGrossWeight()
+			throws TrainException {
+		testLocomotive = new Locomotive(ZERO, DEFAULT_LOCO_CLASS);
 	}
 
 	/*
@@ -86,14 +96,12 @@ public class RollingStockTests {
 		// TODO make this go through every character, or atleast alot?
 	}
 
-	@Test
-	public void testFreightCarReturnValidGoodsType() throws TrainException {
-		testFreightCar = new FreightCar(DEFAULT_GROSS_WEIGHT,
-				DEFAULT_FREIGHT_TYPE);
-		assertEquals("Return of valid goods type", DEFAULT_FREIGHT_TYPE,
-				testFreightCar.goodsType());
-	}
-
+	/**
+	 * The return of FreightCar's GoodsType() should be the same as the
+	 * goodsType first used in Construction.
+	 * 
+	 * @throws TrainException
+	 */
 	@Test
 	public void testFreightCarReturnGoodsType() throws TrainException {
 		testFreightCar = new FreightCar(DEFAULT_GROSS_WEIGHT,
@@ -102,6 +110,12 @@ public class RollingStockTests {
 				DEFAULT_FREIGHT_TYPE, testFreightCar.goodsType());
 	}
 
+	/**
+	 * The human readable representation of a FreightCar should be like
+	 * 'Freight(G)'.
+	 * 
+	 * @throws TrainException
+	 */
 	@Test
 	public void testFreightTrainToString() throws TrainException {
 		testFreightCar = new FreightCar(DEFAULT_GROSS_WEIGHT,
@@ -112,8 +126,76 @@ public class RollingStockTests {
 				testFreightCar.toString());
 	}
 
-	// copy of test
+	/*
+	 * Locomotive specific tests
+	 */
+	// TODO comment these locomotive tests
+	
+	@Test (expected = TrainException.class)
+	public void testLocomotiveWithInvalidClassPower() throws TrainException {
+		String invalidPower = "10";
+		String electricEngine = "E";
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, invalidPower = electric);
+	}
+	
+	@Test (expected = TrainException.class)
+	public void testLocomotiveWithInvalidClassEngine() throws TrainException {
+		String power = "5";
+		String invalidHydrogenEngine = "H";
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, power + invalidHydrogenEngine);
+	}
+	
 	@Test
+	public void testLocomotiveWithMaxPowerClass() throws TrainException {
+		String validMaxPowerClass = "9E";
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, validMaxPowerClass);
+	}
+	
+	@Test
+	public void testLocomotiveWithMinPowerClass() throws TrainException {
+		String validMinPowerClass = "1E";
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, validMinPowerClass);
+	}
+	
+	@Test
+	public void testLocomotiveAllValidClassEngineTypes() throws TrainException {
+		String power = "5";
+		String electric = "E";
+		String diesel = "D";
+		String steam = "S";
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, power + electric);
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, power + diesel);
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, power + steam);
+	}
+	
+	@Test
+	public void testLocomotiveReturnValidPower() throws TrainException {
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, DEFAULT_LOCO_CLASS);
+		assertEquals("Returned power should be " + (DEFAULT_LOCO_POWER * 100), DEFAULT_LOCO_POWER * 100, testLocomotive.power());		
+	}
+	
+	@Test
+	public void testLocomotiveReturnValidMaxPower() throws TrainException {
+		Integer validMaxPower = 9;
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, validMaxPower + DEFAULT_LOCO_ENGINE);
+		assertEquals(validMaxPower * 100, testLocomotive.power());
+	}
+	
+	@Test
+	public void testLocomotiveReturnValidMinPower() throws TrainException {
+		Integer validMinPower = 1;
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, validMinPower + DEFAULT_LOCO_ENGINE);
+		assertEquals(validMinPower * 100, testLocomotive.power());
+	}
+	
+	@Test
+	public void testLocomotiveToString() throws TrainException {
+		testLocomotive = new Locomotive(DEFAULT_GROSS_WEIGHT, DEFAULT_LOCO_CLASS);
+		assertEquals("Representation of Locomotive should be like 'Loco(x)'.", "Loco(" + DEFAULT_LOCO_CLASS + ")", testLocomotive.toString());
+	}
+	
+	// copy of test
+	@Test (expected = TrainException.class)
 	public void testtest() throws TrainException {
 
 	}
