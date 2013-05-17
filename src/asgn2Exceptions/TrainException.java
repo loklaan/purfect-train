dcc0f97cd6dd7c6570f84107e7482f99d8ed85a8
Lochlan Bunn <loklaan@gmail.com>
2013-05-17 17:01:22 +1000

    TrainException
    Added exception for the train stuff. Followed spec and implemented
    Serializable.. Tbh I'm now sure what this helps with; maybe it's
    preventing returing binary jargon? *Shurg*

---
+++ src/asgn2Exceptions/TrainException.java
@@ -0,0 +1,22 @@
+package asgn2Exceptions;
+
+import java.io.Serializable;
+
+/**
+ * A simple class for exceptions thrown by railway shunting and boarding operations.
+ * @author Lochlan Bunn - 8509719
+ *
+ */
+
+public class TrainException extends Exception implements Serializable {
+
+	/**
+	 * Constructs a new TrainException object.
+	 * 
+	 * @param message
+	 * an informative message describing the cause of the problem
+	 */
+	public TrainException(String message) {
+		super("Train Exception: " + message);
+	}
+}
