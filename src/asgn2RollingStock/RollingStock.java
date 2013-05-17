84c4d9ecbf8f5cd23e272d39f58def987676238e
Murray Coggan <muzza74@gmail.com>
2013-05-17 19:33:18 +1000

    Initial RollingStock abstract class
    Includes:
    	constructor
    	GetGrossWeight
    	toString abstract

--- src/asgn2RollingStock/RollingStock.java
+++ src/asgn2RollingStock/RollingStock.java
@@ -1,5 +1,36 @@
 package asgn2RollingStock;
 
+import asgn2Exceptions.TrainException;
+
+/**
+ * Rolling stock are the individual carriages from which a train is constructed.
+ * This abstract class defines characteristics which they all share, most
+ * notably having a known gross weight, measured here in tonnes. (There are, of
+ * course many other important shared characteristics of railway carriages, such
+ * as identifying codes, a certain number of wheels, the track gauge they're
+ * designed for, etc, but we don't need these for this assignment.)
+ * 
+ * @author Murray Coggan - 8291951
+ * 
+ */
 public abstract class RollingStock {
+
+	private Integer grossWeight;
+
+	public RollingStock(Integer grossWeight) throws TrainException {
+		if (grossWeight < 1) {
+			throw new TrainException(
+					"The grossWeight must be a positive value, above 0.");
+		} else {
+			this.grossWeight = grossWeight;
+		}
+	}
+
+	public Integer getGrossWeight() {
+		return this.grossWeight;
+	}
+
+	@Override
+	public abstract String toString();
 
 }
