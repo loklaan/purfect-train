59b869deb698629eed6cfc691a0b1184b6eae371
Lochlan Bunn <loklaan@gmail.com>
2013-05-20 21:45:49 +1000

    DepartingTrain nextCarriage / firstCarriage

--- src/asgn2Train/DepartingTrain.java
+++ src/asgn2Train/DepartingTrain.java
@@ -1,7 +1,6 @@
 package asgn2Train;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import asgn2Exceptions.TrainException;
@@ -10,10 +9,48 @@ import asgn2RollingStock.Locomotive;
 import asgn2RollingStock.PassengerCar;
 import asgn2RollingStock.RollingStock;
 
+/**
+ * <p>
+ * A train is a sequence of carriages. This class defines various operations
+ * that can be performed to prepare a long-distance train for departure.
+ * </p>
+ * 
+ * <p>
+ * We assume that a train can be assembled from any available rolling stock,
+ * including locomotives, passenger cars and freight cars. However, they may be
+ * configured in only a certain sequence:
+ * </p>
+ * 
+ * <ol>
+ * <li>The first carriage must be a locomotive (and there can be only one
+ * locomotive per train).</li>
+ * <li>This may be followed by zero or more passenger cars.</li>
+ * <li>These may be followed by zero or more freight cars.
+ * </ol>
+ * <p>
+ * Any other configurations of rolling stock are disallowed.
+ * </p>
+ * 
+ * <p>
+ * The process of preparing the train for departure occurs in two stages:
+ * </p>
+ * 
+ * <ol>
+ * <li>The train is assembled from individual carriages. New carriages may be
+ * added to the rear of the train only. (Similarly, carriages may be removed
+ * from the rear of the train only.)</li>
+ * <li>Passengers board the train. For safety reasons, no carriage shunting
+ * operations may be performed when any passengers are on board the train.</li>
+ * </ol>
+ * 
+ * @author Lochlan Bunn - 8509719
+ * 
+ */
 public class DepartingTrain {
 	private List<RollingStock> carriages;
 	private Integer numberOnBoard;
 	private Integer numberOfSeats;
+	private Integer carriagesIterator;
 
 	/**
 	 * Constructs a (potential) train object containing no carriages (yet).
@@ -22,6 +59,7 @@ public class DepartingTrain {
 		this.carriages = new ArrayList<RollingStock>();
 		this.numberOfSeats = 0;
 		this.numberOnBoard = 0;
+		this.carriagesIterator = -1;
 	}
 
 	/**
@@ -49,6 +87,7 @@ public class DepartingTrain {
 						"Locomotive must be added as the first carriage.");
 			} else {
 				carriages.add(newCarriage);
+				this.carriagesIterator = 0;
 			}
 		} else if (newCarriage instanceof FreightCar) {
 			carriages.add(newCarriage);
@@ -66,16 +105,73 @@ public class DepartingTrain {
 	}
 
 	/**
+	 * Returns the first carriage on the train (which must be a locomotive).
+	 * Special value null is returned if there are no carriages on the train at
+	 * all.<br/>
+	 * <br/>
+	 * 
+	 * NB: When combined with method nextCarriage, this method gives us a simple
+	 * ability to iteratively examine each of the train's carriages.
+	 * 
+	 * @return The first carriage in the train, or null if there are no
+	 *         carriages.
+	 */
+	public RollingStock firstCarriage() {
+		if (isTrainEmpty()) {
+			return null;
+		} else {
+			this.carriagesIterator = 0;
+			return carriages.get(this.carriagesIterator);
+		}
+	}
+
+	/**
+	 * Returns the next carriage in the train after the one returned by the
+	 * immediately preceding call to either this method or method firstCarriage.
+	 * Special value null is returned if there is no such carriage. If there has
+	 * been no preceding call to either firstCarriage or nextCarriage, this
+	 * method behaves like firstCarriage, i.e., it returns the first carriage in
+	 * the train, if any.<br/>
+	 * <br/>
+	 * 
+	 * NB: When combined with method firstCarriage, this method gives us a
+	 * simple ability to iteratively examine each of the train's carriages.
+	 * 
+	 * @return The train's next carriage after the one returned by the
+	 *         immediately preceding call to either firstCarriage or
+	 *         nextCarriage, or null if there is no such carriage.
+	 */
+	public RollingStock nextCarriage() {
+		this.carriagesIterator++;
+		try {
+			// TODO: acts just like firstCarriage, after a ++ on -1 (could
+			// refactor?)
+			return carriages.get(carriagesIterator);
+		} catch (IndexOutOfBoundsException e) { // 'next' element does not exist
+			return null;
+		}
+	}
+
+	/**
 	 * Returns the last carriage in the train, unless there is none where it
 	 * returns null
 	 * 
 	 * @return Last carriage or null if one does not exist
 	 */
 	private RollingStock getLastCarriage() {
-		if (carriages.size() != 0) {
+		if (isTrainEmpty()) {
 			return carriages.get(carriages.size() - 1);
 		}
 		return null;
+	}
+
+	/**
+	 * Returns if the train has no carriages.
+	 * 
+	 * @return If the train is empty of carriages.
+	 */
+	private boolean isTrainEmpty() {
+		return carriages.size() != 0; // or carriages.isEmpty()?
 	}
 
 	/**
@@ -116,4 +212,5 @@ public class DepartingTrain {
 			return passengers;
 		}
 	}
+
 }
