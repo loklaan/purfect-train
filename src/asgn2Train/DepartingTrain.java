027ed02fa1e6aa838ba57844e835375c5d1b67ba
Lochlan Bunn <loklaan@gmail.com>
2013-05-20 20:10:11 +1000

    DepartingTrain method board
    Also private method getLastCarriage

--- src/asgn2Train/DepartingTrain.java
+++ src/asgn2Train/DepartingTrain.java
@@ -1,6 +1,7 @@
 package asgn2Train;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import asgn2Exceptions.TrainException;
@@ -11,12 +12,16 @@ import asgn2RollingStock.RollingStock;
 
 public class DepartingTrain {
 	private List<RollingStock> carriages;
+	private Integer numberOnBoard;
+	private Integer numberOfSeats;
 
 	/**
 	 * Constructs a (potential) train object containing no carriages (yet).
 	 */
 	public DepartingTrain() {
 		this.carriages = new ArrayList<RollingStock>();
+		this.numberOfSeats = 0;
+		this.numberOnBoard = 0;
 	}
 
 	/**
@@ -34,24 +39,81 @@ public class DepartingTrain {
 	public void addCarriage(RollingStock newCarriage) throws TrainException {
 		if (!(newCarriage instanceof Locomotive) && carriages.size() == 0) {
 			throw new TrainException("First carriage must be a Locomotive");
+		} else if (this.numberOnBoard > 0) {
+			throw new TrainException(
+					"Cannot add carriages once passengers are aboard the train.");
 		}
 		if (newCarriage instanceof Locomotive) {
 			if (carriages.size() > 1) {
 				throw new TrainException(
-						"Locomotive must be added as the first carriage.");	
+						"Locomotive must be added as the first carriage.");
 			} else {
 				carriages.add(newCarriage);
 			}
 		} else if (newCarriage instanceof FreightCar) {
 			carriages.add(newCarriage);
 		} else if (newCarriage instanceof PassengerCar) {
-			if (carriages.size() > 1
-					&& carriages.get(carriages.size() - 1) instanceof FreightCar) {
+			if (carriages.size() > 1 && getLastCarriage() instanceof FreightCar) {
 				throw new TrainException(
 						"Passengers must be added after a Locomotive and before FreightCars.");
 			}
 			carriages.add(newCarriage);
+			this.numberOfSeats += ((PassengerCar) getLastCarriage())
+					.numberOfSeats();
+			this.numberOnBoard += ((PassengerCar) getLastCarriage())
+					.numberOnBoard();
 		}
 	}
 
+	/**
+	 * Returns the last carriage in the train, unless there is none where it
+	 * returns null
+	 * 
+	 * @return Last carriage or null if one does not exist
+	 */
+	private RollingStock getLastCarriage() {
+		if (carriages.size() != 0) {
+			return carriages.get(carriages.size() - 1);
+		}
+		return null;
+	}
+
+	/**
+	 * Adds the given number of people to passenger carriages on the train. We
+	 * do not specify where the passengers must sit, so they can be allocated to
+	 * any vacant seat in any passenger car.
+	 * 
+	 * @param newPassengers
+	 *            The number of people wish to board the train.
+	 * @return The number of people who were unable to board the train because
+	 *         they couldn't get a seat.
+	 * @throws TrainException
+	 *             If the number of new passengers is negative.
+	 */
+	public Integer board(Integer newPassengers) throws TrainException {
+		Integer passengers = newPassengers;
+		Integer leftOverPassengers;
+		if (passengers < 0) {
+			throw new TrainException(
+					"Amount of new passengers must not be negative");
+		} else {
+			// Iterate over carriages until a freightcar
+			for (RollingStock carriage : carriages) {
+				// try to board passengers onto a passengercar, dealing with the
+				// left over passengers
+				if (carriage instanceof PassengerCar) {
+					leftOverPassengers = (Integer) ((PassengerCar) carriage)
+							.board(passengers);
+					this.numberOnBoard += passengers - leftOverPassengers;
+					passengers = leftOverPassengers;
+				} else if (carriage instanceof FreightCar) {
+					break;
+				}
+				if (passengers == 0) {
+					break;
+				}
+			}
+			return passengers;
+		}
+	}
 }
