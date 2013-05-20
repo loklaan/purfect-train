78a08e125a243f0649def9a1c662c51cda37854a
Lochlan Bunn <loklaan@gmail.com>
2013-05-20 16:35:16 +1000

    Initial Departing Train
    Constructor, carriages data structure & addCarriage

--- src/asgn2Train/DepartingTrain.java
+++ src/asgn2Train/DepartingTrain.java
@@ -1,5 +1,51 @@
 package asgn2Train;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import asgn2Exceptions.TrainException;
+import asgn2RollingStock.FreightCar;
+import asgn2RollingStock.Locomotive;
+import asgn2RollingStock.PassengerCar;
+import asgn2RollingStock.RollingStock;
+
 public class DepartingTrain {
+	private List<RollingStock> carriages;
+
+	/**
+	 * Constructs a (potential) train object containing no carriages (yet).
+	 */
+	public DepartingTrain() {
+		this.carriages = new ArrayList<RollingStock>();
+	}
+
+	/**
+	 * Adds a new carriage to the end of the train. However, a new carriage may
+	 * be added only if the resulting train configuration is valid, as per the
+	 * rules listed above. Furthermore, shunting operations may not be performed
+	 * if there are passengers on the train.
+	 * 
+	 * @param newCarriage
+	 *            The new carriage to be added.
+	 * @throws TrainException
+	 *             If adding the new carriage would produce an invalid train
+	 *             configuration, or if there are passengers on the train.
+	 */
+	public void addCarriage(RollingStock newCarriage) throws TrainException {
+		// TODO make a class to check config against carriage?
+		if (newCarriage instanceof Locomotive && carriages.size() > 1) {
+			throw new TrainException(
+					"Locomotive must be added as the first carriage.");
+		} else if (newCarriage instanceof FreightCar) {
+			carriages.add(newCarriage);
+		} else if (newCarriage instanceof PassengerCar) {
+			if (carriages.size() > 1
+					&& carriages.get(carriages.size() - 1) instanceof FreightCar) {
+				throw new TrainException(
+						"Passengers must be added after a Locomotive and before FreightCars.");
+			}
+			carriages.add(newCarriage);
+		}
+	}
 
 }
