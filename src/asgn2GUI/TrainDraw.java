127fb10412b7aad27a4f76bf5a514fbfdc4a5973
Murray Coggan <muzza74@gmail.com>
2013-05-27 01:19:19 +1000

    Commented TrainDraw

--- src/asgn2GUI/TrainDraw.java
+++ src/asgn2GUI/TrainDraw.java
@@ -20,6 +20,8 @@ public class TrainDraw extends JPanel {
 	final static int HEIGHT = 200;
 	final int CARRIAGE_HEIGHT = 50;
 	final int CARRIAGE_WIDTH = 100;
+	// distance used to iterate drawPos, will make carriages
+	// draw 10 pixels apart from each other
 	final int DISTANCE = CARRIAGE_WIDTH + 10;
 	private int drawPos = 0;
 	private static DepartingTrain theTrain;
@@ -30,6 +32,12 @@ public class TrainDraw extends JPanel {
 	public TrainDraw() {
 	}
 	
+	/* (non-Javadoc)
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 * 
+	 * Call repaint() to utilise this function
+	 * 
+	 */
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g); 
 		if (theTrain != null){
@@ -37,6 +45,13 @@ public class TrainDraw extends JPanel {
 		}
 	}
 	
+	
+	/**
+	 * @param theTrain
+	 * 
+	 * Sets the train to be displayed to the passed train
+	 * 
+	 */
 	public void SetTrain(DepartingTrain theTrain){
 		this.theTrain = theTrain;
 		RollingStock tempCar = theTrain.firstCarriage();
@@ -47,6 +62,13 @@ public class TrainDraw extends JPanel {
 		
 	}
 	
+	/**
+	 * Draws the currently set train as a series of rectangles, with information
+	 * relating to each train carriage displayed on the side.
+	 *
+	 * @param g
+	 * 		The Graphics used to draw the train.
+	 */
 	private void drawTrain(Graphics g){
 		RollingStock currentCar = theTrain.firstCarriage();
 		while (currentCar != null){
@@ -64,11 +86,23 @@ public class TrainDraw extends JPanel {
 	}
 	
 	
+	/**
+	 * Draws the passed Locomotive, along with two small bars representing the power,
+	 * and the weight of the train relative to the power. Also draws text representing
+	 * the power of the locomotive and the total weight of the train. 
+	 * 
+	 * @param g
+	 * 		The Graphics used to draw the Locomotive
+	 * @param theLoco
+	 * 		The Locomotive to be drawn
+	 */
 	private void drawLoco(Graphics g, Locomotive theLoco){
 		g.setColor(Color.BLUE);
 		g.drawRect(drawPos, 0, CARRIAGE_WIDTH, CARRIAGE_HEIGHT);
 		g.fillRect(drawPos, 0, CARRIAGE_WIDTH, CARRIAGE_HEIGHT);
 		
+		// Draws the small bars representing the power and relative weight of the 
+		// Locomotive
 		int barHeight = 7;
 		int weightBarLength = 0;
 		int barOffset = 5;
@@ -80,6 +114,8 @@ public class TrainDraw extends JPanel {
 		if (theLoco.power() < totalWeight){
 			weightBarLength = CARRIAGE_WIDTH;
 		} else {
+			// Calculates the length of the lower bar as a percentage of the
+			// power of the train
 			float tempLength = ((float)totalWeight/(float)theLoco.power()*100);
 			weightBarLength = (int)tempLength;
 		}
@@ -88,8 +124,19 @@ public class TrainDraw extends JPanel {
 		g.fillRect(drawPos, CARRIAGE_HEIGHT + barOffset, weightBarLength, barHeight);
 	}
 	
+	
+	/**
+	 * Draws the specified PassengerCar, along with text on the carriage specifying the 
+	 * weight of the car, and the number of passengers and the number of available seats 
+	 * in the format passengers/seats
+	 * 
+	 * @param g
+	 * 		The Graphics used to draw the PassengerCar
+	 * @param thePassenger
+	 * 		The PassengerCar to be drawn
+	 */
 	private void drawPassenger(Graphics g, PassengerCar thePassenger){
-		g.setColor(Color.RED);
+		g.setColor(Color.YELLOW);
 		g.drawRect(drawPos, 0, CARRIAGE_WIDTH, CARRIAGE_HEIGHT);
 		g.fillRect(drawPos, 0, CARRIAGE_WIDTH, CARRIAGE_HEIGHT);
 		PassengerCar tempCarriage = thePassenger;
@@ -104,6 +151,13 @@ public class TrainDraw extends JPanel {
 				(CARRIAGE_HEIGHT/2) + stringHOffset);
 	}
 	
+	
+	/**
+	 * Draws the specified FreightCar, along with text on the carriage specifying the
+	 * type of freight carried and the weight of the car.
+	 * @param g
+	 * @param theFreight
+	 */
 	private void drawFreight(Graphics g, FreightCar theFreight){
 		g.setColor(Color.GREEN);
 		g.drawRect(drawPos, 0, CARRIAGE_WIDTH, CARRIAGE_HEIGHT);
