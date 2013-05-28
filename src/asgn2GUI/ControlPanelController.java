f0a52c770dc36bed6e96754e59516019f113b220
Lochlan Bunn <loklaan@gmail.com>
2013-05-28 15:59:37 +1000

    Fixed braces in controller

--- src/asgn2GUI/ControlPanelController.java
+++ src/asgn2GUI/ControlPanelController.java
@@ -35,64 +35,64 @@ public class ControlPanelController {
 		public void actionPerformed(ActionEvent e) {
 
 		}
+	}
 
-		/**
-		 * When a request is made for a carriage to be added to a train, the
-		 * following is read and computed before addage:
-		 * <ul>
-		 * <li>check if can shunt</li>
-		 * <li>weight</li>
-		 * <li>car type</li>
-		 * <li><em>relevant</em> car properties</li>
-		 * <li>update train canvas</li>
-		 * </ul>
-		 */
-		class AddCarriageListener implements ActionListener {
+	/**
+	 * When a request is made for a carriage to be added to a train, the
+	 * following is read and computed before addage:
+	 * <ul>
+	 * <li>check if can shunt</li>
+	 * <li>weight</li>
+	 * <li>car type</li>
+	 * <li><em>relevant</em> car properties</li>
+	 * <li>update train canvas</li>
+	 * </ul>
+	 */
+	class AddCarriageListener implements ActionListener {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-
-		}
-
-		/**
-		 * When a request is made for a carriage to be removed from a train, the
-		 * following is done:
-		 * <ul>
-		 * <li>check if can shunt</li>
-		 * <li>remove last carriage</li>
-		 * <li>update train canvas</li>
-		 * </ul>
-		 */
-		class RemoveCarriageListener implements ActionListener {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-
-		}
-
-		/**
-		 * When a request is made for a number of passengers to be added to a
-		 * train, the following is done:
-		 * <ul>
-		 * <li>does train have passenger cars</li>
-		 * <li>is train full</li>
-		 * <li>add passengers</li>
-		 * <li>update conductor metrics</li>
-		 * <li>update train canvas</li>
-		 * </ul>
-		 */
-		class AddPassengerListener implements ActionListener {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
+		@Override
+		public void actionPerformed(ActionEvent e) {
 
 		}
 
 	}
+
+	/**
+	 * When a request is made for a carriage to be removed from a train, the
+	 * following is done:
+	 * <ul>
+	 * <li>check if can shunt</li>
+	 * <li>remove last carriage</li>
+	 * <li>update train canvas</li>
+	 * </ul>
+	 */
+	class RemoveCarriageListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+
+	}
+
+	/**
+	 * When a request is made for a number of passengers to be added to a train,
+	 * the following is done:
+	 * <ul>
+	 * <li>does train have passenger cars</li>
+	 * <li>is train full</li>
+	 * <li>add passengers</li>
+	 * <li>update conductor metrics</li>
+	 * <li>update train canvas</li>
+	 * </ul>
+	 */
+	class AddPassengerListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+
+	}
+
 }
