f35a93d3df490594b171a7748181d3ddfdb15bdc
Lochlan Bunn <loklaan@gmail.com>
2013-05-28 22:48:15 +1000

    Comments for main program.

--- src/asgn2GUI/ControlPanel.java
+++ src/asgn2GUI/ControlPanel.java
@@ -1,7 +1,8 @@
 package asgn2GUI;
 
 /**
- * Main program for the Control Panel
+ * Main program for the Control Panel. This control panel allows Train Drivers
+ * and Conductors to create and manage a train that is soon to depart.
  * 
  * @author Lochlan Bunn - 8509719
  * 
@@ -10,11 +11,6 @@ public class ControlPanel {
 
 	private static final String PROGRAM_TITLE = "Train Control Panel";
 
-	/**
-	 * Comments.
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
 
 		ControlPanelModel model = new ControlPanelModel();
